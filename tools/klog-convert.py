460b63bfc7c793c61feef1818b81cfb40321d39c
Ralf Ramsauer <ralf@binary-kitchen.de>
2018-11-07 13:13:51 +0000

    klog-convert: add proper license statement
    Signed-off-by: Ralf Ramsauer <ralf@binary-kitchen.de>

--- tools/klog-convert.py
+++ tools/klog-convert.py
@@ -3,9 +3,18 @@
 """
 klog-convert - Convert the Dokuwiki kitchenlog sites to an abstract format
 
-Author: Ralf Ramsauer <ralf@binary-kitchen.de>
+Copyright (c) Binary Kitchen e.V., 2018
 
-Licensed under the terms and conditions of the GPLv2.
+Author:
+  Ralf Ramsauer <ralf@binary-kitchen.de>
+
+This work is licensed under the terms of the GNU GPL, version 2.  See
+the LICENSE file in the top-level directory.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
 """
 
 import dateparser
